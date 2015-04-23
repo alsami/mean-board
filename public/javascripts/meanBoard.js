7a84362289f134dd78b3ca0310dfa8575d4c6958
unknown <sami.al-khatib@W0009.xax.de>
2015-04-23 16:49:40 +0200

    a lot changes were made

--- public/javascripts/meanBoard.js
+++ public/javascripts/meanBoard.js
@@ -1,36 +1,104 @@
 var app = angular.module('meanBoard', ['ngRoute']);
 
-app.config(['$routeProvider', function($routeProvider){
-	/*
+app.config(['$routeProvider', 
+	function($routeProvider){
 	$routeProvider
 		.when('/', {
-			templateUrl: './partials/start.html',
-			controller: 'RegisterCtrl'
-		})
-		.when('/login', {
-			templateUrl: './partials/login.html',
-			controller: 'RegisterCtrl'
-		})
-		.when('/register', {
-			templateUrl: './partials/register.html',
-			controller: 'RegisterCtrl'
+			templateUrl: './partials/category.html',
+			controller: 'categoryCtrl'
+			/*,
+			resolve: {
+				postPromise: ['categoryFactory'],
+					function(categoryFactory){
+						return categoryFactory.getAllCategories();
+					}
+			}
+			*/
 		})
 		.otherwise({redirectTo: '/'});
-	*/
-}]);
-
-app.controller('RegisterCtrl', ['$scope', '$http', function($scope, $http){
-	$scope.newUser = {};
-	$scope.registerUser = function(){
-		$http({
-			method: 'POST',
-			url: '/api/user',
-			data: $scope.newUser
-		}).success(function (data, status, headers, config) {
-			$scope.newUser = {}
-			console.log(data);
-		}).error(function (data, status, headers, config) {
-			alert(data);
-		});
 	}
-}]);
+]);
+
+app.factory('categoryFactory', ['$http', 
+	function($http){
+		var categoryObject = {
+			categories: [
+				{				
+					title: 'Category 1',
+					parent: null,
+					categories: {},
+					threads: {},
+					lastPost: {},
+					deletedAt: null
+				},
+				{				
+					title: 'Category 2',
+					parent: null,
+					categories: {},
+					threads: {},
+					lastPost: {},
+					deletedAt: null
+				},
+				{				
+					title: 'Category 3',
+					parent: null,
+					categories: {},
+					threads: {},
+					lastPost: {},
+					deletedAt: null
+				},
+				{				
+					title: 'Category 4',
+					parent: null,
+					categories: {},
+					threads: {},
+					lastPost: {},
+					deletedAt: null
+				},
+				{				
+					title: 'Category 5',
+					parent: null,
+					categories: {},
+					threads: {},
+					lastPost: {},
+					deletedAt: null
+				}
+			]
+		}
+		
+		categoryObject.getCategoryById = function(categoryId){
+			return $http.get('/api/category' + categoryId).then(
+				function(res){
+					return res.data;
+				}
+			)
+		}
+		
+		categoryObject.getAllCategories = function(){
+			return $http.get('/api/category').success(
+				function(data){
+					angular.copy(data, categoryObject.categories);
+				}
+			)
+		}
+		
+		categoryObject.updateCategoryById = function(category){
+			$http.put('/api/' + category._id, category);
+		}
+		
+		categoryObject.deleteCategoryById = function(categoryId){
+			return $http.delete('/api/' + categoryId);
+		}
+		
+		return categoryObject;
+	}
+])
+
+app.controller('categoryCtrl', [
+	'$scope', 
+	'$http', 
+	'categoryFactory',
+	function($scope, $http, categoryFactory){
+		$scope.categories = categoryFactory.categories;
+	}
+]);
