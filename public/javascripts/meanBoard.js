76e5f79c19a7d3a136d753ce8b96fb83d69cc587
NubCoda <al.khatib.sami1990@googlemail.com>
2015-04-26 19:25:45 +0200

    quite a lot changes. Can't remember

--- public/javascripts/meanBoard.js
+++ public/javascripts/meanBoard.js
@@ -1,40 +1,47 @@
 var app = angular.module('meanBoard', ['ui.router']);
 
 app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider){
-	$urlRouterProvider.otherwise('/home');
+	//$urlRouterProvider.otherwise('/home');
 	
 	$stateProvider
-		.state('/category', {
-			url: '/category',
-			templateUrl: './partials/category.html',
-			controller: 'categoryCtrl',
+		.state('board', {
+			url: '/board',
+			templateUrl: './partials/board.html',
+			controller: 'boardCtrl',
 			resolve:{
 				postPromise: ['categoryFactory', function(categoryFactory){
 					return categoryFactory.getAllCategories();
 				}]
 			}
-			
 		})
-		.state('/home', {
+		.state('category',{
+			url: '/category/{id}',
+			templateUrl: './partials/category.html',
+			controller: 'categoryCtrl',
+			resolve: {
+				category: ['$stateParams', 'categoryFactory', function($stateParams, categoryFactory) {
+					return categoryFactory.getCategory($stateParams.id);
+				}]
+			}
+		})
+		.state('home', {
 			url: '/home',
 			templateUrl: './partials/home.html'
-		})
+		});
 }]);
 
 app.factory('categoryFactory', ['$http', function($http){
 	var categoryObject = {
-		categories : [/*{title: 'hallo'}*/]
-	}
-	
-	categoryObject.getAllCategories = function(){
-		return $http.get('/api/category').success(function(data){
-			angular.copy(data, categoryObject.categories);
-		});
+		categoryJSON : [/*{title: 'hallo'}*/]
 	}
 	
 	categoryObject.createCategory = function(category){
 		return $http.post('/api/category', category).success(function(data){
-			categoryObject.categories.push(data);
+			if(category.parent == null){
+				categoryObject.categoryJSON.push(data)
+			}else{
+				categoryObject.categoryJSON[0].categories.push(data)
+			}
 		});
 	}
 	
@@ -46,7 +53,22 @@ app.factory('categoryFactory', ['$http', function($http){
 	
 	categoryObject.deleteCategory = function(category){
 		return $http.delete('/api/category/' + category._id, category).success(function(data){
-			categoryObject.getAllCategories();
+			return true;
+		})
+		.error(function(){
+			return false;
+		});
+	}
+	
+	categoryObject.getCategory = function(categoryId){
+		return $http.get('/api/category/' + categoryId).success(function(res){
+			return res.data;
+		});
+	}
+	
+	categoryObject.getAllCategories = function(){
+		return $http.get('/api/category').success(function(data){
+			angular.copy(data, categoryObject.categoryJSON);
 		});
 	}
 
@@ -54,15 +76,10 @@ app.factory('categoryFactory', ['$http', function($http){
 }]);
 
 
+
 app.factory('userFactory', ['$http', function($http){
 	var userObject = {
 		users : []
-	}
-	
-	userObject.getAllUsers = function(){
-		return $http.get('/api/user').success(function(data){
-			angular.copy(data, userObject.users);
-		});
 	}
 	
 	userObject.createUser = function(user){
@@ -72,7 +89,15 @@ app.factory('userFactory', ['$http', function($http){
 	}
 	
 	userObject.updateUser = function(user){
+		return $http.put('/api/user/' + user._id, user).success(function(data){
+			userObject.getAllUsers();
+		});
+	}
 	
+	userObject.getAllUsers = function(){
+		return $http.get('/api/user').success(function(data){
+			angular.copy(data, userObject.users);
+		});
 	}
 	
 	return userObject;
@@ -88,10 +113,10 @@ app.controller('RegisterCtrl', ['$scope', 'userFactory', function($scope, userFa
 		}
 }]);
 
-app.controller('categoryCtrl', ['$scope', 'categoryFactory',function($scope, categoryFactory){
-	$scope.categories = categoryFactory.categories;
+app.controller('boardCtrl', ['$scope', 'categoryFactory', function($scope, categoryFactory){
+	$scope.categoryJSON = categoryFactory.categoryJSON;
 	$scope.newCategory = {};
-	$scope.createMainCategory = function(){
+	$scope.createCategory = function(){
 		categoryFactory.createCategory($scope.newCategory);
 		$scope.newCategory = {};
 	}
@@ -99,6 +124,12 @@ app.controller('categoryCtrl', ['$scope', 'categoryFactory',function($scope, cat
 		categoryFactory.updateCategory(category);
 	}
 	$scope.deleteCategory = function(category){
-		categoryFactory.deleteCategory(category);
+		if(categoryFactory.deleteCategory(category)){
+			$scope.categoryJSON.splice($scope.categoryJSON.indexOf(category), 1);
+		}
 	}
+}]);
+
+app.controller('categoryCtrl', ['$scope', 'categoryFactory', 'category', function($scope, categoryFactory, category){
+	$scope.category = category;
 }]);
