141f3a1e2efd1cbaa6129d76ba99f91322430811
iLoveSalt <kramer.a@gmx.net>
2015-04-29 11:00:37 +0200

    Adapted the user factory to be more consistent

--- public/javascripts/user.js
+++ public/javascripts/user.js
@@ -2,35 +2,45 @@ var userElements = angular.module('user', [])
 
 userElements.factory('userFactory', ['$http', function($http){
   var userObject = {
-    user : []
+    user : null
   }
 
-  userObject.loginUser = function(user){
+  userObject.login = function(user){
     $http.post('/api/user/login', user)
     .success(function(data){
-        console.log("I am in");
-        userObject.user.push(data);
+        userObject.user = data;
     })
     .error(function(error){
-      console.log(error)
+		alert(error);
     });
+  };
+
+  userObject.logout = function(){
+	$http.get('/api/user/logout')
+	.success(function(data){
+		alert(data);
+	});
   }
 
-  userObject.createUser = function(user){
-    return $http.post('/api/user', user)
+  userObject.create = function(user){
+    $http.post('/api/user', user)
     .success(function(data){
-        userToLogin = {userName : user.userName, password : user.password};
-        console.log(data.password);
-        userObject.loginUser(userToLogin);
-    });
-  }
+    	userObject.user = data;
+    })
+	.error(function(error){
+		alert(error);
+	});
+  };
 
-  userObject.updateUser = function(user){
+  userObject.update = function(user){
     return $http.put('/api/user/' + user._id, user)
     .success(function(data){
-        return true;
+        userObject.user = data;
+    })
+    .error(function(error){
+    	alert(error);
     });
-  }
+  };
 
   return userObject;
 }]);
