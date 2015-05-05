514fa29829a99922949b8817ab7d255e4339f9b3
Alexander Kramer <kramer.a@gmx.net>
2015-05-05 18:50:39 +0200

    Permission system: Still returns does not work, implemented a callback.

--- routes/lib/permission.js
+++ routes/lib/permission.js
@@ -2,6 +2,7 @@ var Post = require('../../models/Post');
 var Thread = require('../../models/Thread');
 var permission = {};
 
+
 permission.loginRequired = function(req, res, next){
 	if(!req.user){
 		res.status(403).end('Error: Please login!');
@@ -10,35 +11,39 @@ permission.loginRequired = function(req, res, next){
 	}
 };
 
+
 permission.hasPermissionToUpdate = function(req, res, next){
 	if(!req.user){
 		res.status(403).end('Error: Please login to access this route');
 	} else {
-		var requested_object = get_requested_object(req.params.id);
-		if(!requested_object){
-			res.status(404).end('Error: Did not find requested object');
-		} else if(requested_object.createdBy.equals(req.user._id)){
-			next();
-		} else {
-			res.status(403).end('Error: This user do not have the permission to access this route.');
-		}
+		get_requested_object(req.params.id, function(obj){
+			if(!obj){
+				res.status(404).end('Error: Did not find requested object');
+			} else if(obj.createdBy.equals(req.user._id)){
+				next();
+			} else {
+				res.status(403).end('Error: This user do not have the permission to access this route.');
+			}
+		});
 	}
 };
 
 
-function get_requested_object(id){
+function get_requested_object(id, callback){
 	Post.findById(id, function(err, post){
 		if(post){
-			return post;
+			callback(post);
 		} else {
 			Thread.findById(id, function(err, thread){
 				if(thread){
-					return thread;
+					callback(thread);
 				} else {
-					return null;
+					callback(null);
 				}
 			});
 		}
 	});
 };
+
+
 module.exports = permission;
