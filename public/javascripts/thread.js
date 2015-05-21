6638b49c4d27e2c88a5ced7e2746dcdcda2480c5
NubCoda <al.khatib.sami1990@googlemail.com>
2015-05-21 21:24:10 +0200

    Do not merge! A lot of ui changes, new css classes and helper functions for posts and for authuser

--- public/javascripts/thread.js
+++ public/javascripts/thread.js
@@ -90,7 +90,7 @@ threadModule.controller('createThreadCtrl', ['$scope', '$state', 'threadFactory'
 	}
 }]);
 
-threadModule.controller('basicThreadCtrl', ['$scope', 'threadFactory', 'postFactory', 'category', 'thread', function($scope, threadFactory, postFactory, category, thread){
+threadModule.controller('basicThreadCtrl', ['$scope', '$state', 'threadFactory', 'postFactory', 'category', 'thread', function($scope, $state, threadFactory, postFactory, category, thread){
 	$scope.thread = thread.data;
 	$scope.category = category.data;
 	$scope.newPost = { parent : $scope.thread };
@@ -118,6 +118,10 @@ threadModule.controller('basicThreadCtrl', ['$scope', 'threadFactory', 'postFact
 			$scope.thread.posts[$scope.thread.posts.indexOf(post)] = data;
 			$scope.enableEditation(false, null);
 		});
+	}
+
+	$scope.openPostState = function(postId){
+		$state.go('view-post', {'postId' : postId})
 	}
 
 	$scope.deletePost = function(post){
