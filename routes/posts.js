62a6d620a2e2c3faf13b2781bfbfdba139b8ac55
Alexander Kramer <kramer.a@gmx.net>
2015-05-22 10:54:53 +0200

    #15 user got a attribute posts, creating a post will increment and soft delete will decrement this attribute

--- routes/posts.js
+++ routes/posts.js
@@ -8,6 +8,7 @@ var mongoose = require('mongoose');
 var Post = require('../models/Post.js');
 var Thread = require('../models/Thread.js');
 var Category = require('../models/Category.js');
+var User = require('../models/User.js');
 
 
 /* routes for post */
@@ -75,6 +76,9 @@ router.post('/', function(req, res, next) {
 	Post.create(req.body, function(err, post) {
 		if (err) return next(err);
 
+		// increment the posts attribute of the user
+		change_number_of_posts_for_user(req.user._id, 1);
+
 		// register the new post at the parent thread
 		Thread.findByIdAndUpdate(post.parent, { lastPost: post._id, $push : { posts: post}}, function(err, thread){
 			if(err) return next(err);
@@ -86,6 +90,17 @@ router.post('/', function(req, res, next) {
 		return_populated_post(post, res, next);
 	});
 });
+
+
+var change_number_of_posts_for_user = function(userId, number){
+	User.findById(userId, function(err, user){
+		if(err) return next(err);
+		user.posts += number;
+		user.save(function(err){
+			if(err) return next(err);
+		});
+	});
+};
 
 
 var setLastPostForAllCategories = function(categoryId, lastPostId){
@@ -130,6 +145,12 @@ router.put('/:id', permission.check, function(req, res, next) {
 var update_and_return_populated_post = function(id, update_obj, res, next){
 	Post.findByIdAndUpdate(id, update_obj, function (err, post) {
 		if (err) return next(err);
+
+		// decrement the posts attribute of the user for soft delete
+		if(post.deletedAt){
+			change_number_of_posts_for_user(post.createdBy, -1);
+		}
+
 		return_populated_post(post, res, next);
 	});
 };
